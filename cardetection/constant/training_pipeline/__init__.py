e5d952c8552bcd6bfcec00bb818216fa21b8a357
Parvez13 <prvzsohail@gmail.com>
2023-04-30 11:16:34 +0530

    Add files

--- cardetection/constant/training_pipeline/__init__.py
+++ cardetection/constant/training_pipeline/__init__.py
@@ -10,7 +10,7 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/Parvez13/End_to_End_Car_Detection/raw/main/data.zip"
+DATA_DOWNLOAD_URL: str = "https://github.com/Parvez13/End_to_End_Car_Detection/raw/main/cardata.zip"
 
 """
 Data Validation realted contant start with DATA_VALIDATION VAR NAME
--- cardetection/pipeline/training_pipeline.py
+++ cardetection/pipeline/training_pipeline.py
@@ -83,7 +83,6 @@ class TrainPipeline:
             if data_validation_artifact.validation_status == True:
                  model_trainer_artifact = self.start_model_trainer()
             #     #model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact,s3=self.s3_operations)
-
             
             else:
                  raise Exception("Your data is not in correct format")
