a4852a1406a746ba183c4d3a83c4970e7af19a9b
Parvez13 <prvzsohail@gmail.com>
2023-04-30 10:14:30 +0530

    Update data folder

--- cardetection/constant/training_pipeline/__init__.py
+++ cardetection/constant/training_pipeline/__init__.py
@@ -10,4 +10,25 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
-DATA_DOWNLOAD_URL: str = "https://github.com/Parvez13/End_to_End_Car_Detection/blob/main/data/data.zip"
+DATA_DOWNLOAD_URL: str = "https://github.com/Parvez13/End_to_End_Car_Detection/raw/main/data.zip"
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "data.yaml"]
+
+"""
+MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov5s.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 2
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
