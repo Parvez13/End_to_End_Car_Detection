76f3b2ceb857ebdcc3ea972d97e1a2729e71efc6
Parvez13 <prvzsohail@gmail.com>
2023-05-05 08:47:57 +0530

    Update dataset

--- cardetection/components/model_trainer.py
+++ cardetection/components/model_trainer.py
@@ -38,7 +38,7 @@ class ModelTrainer:
             with open(f'yolov5/models/custom_{model_config_file_name}.yaml', 'w') as f:
                 yaml.dump(config, f)
 
-            os.system(f"cd yolov5/ && python train.py --img 416 --batch {self.model_trainer_config.batch_size} --epochs {self.model_trainer_config.no_epochs} --data ../data.yaml --cfg ./models/custom_yolov5s.yaml --weights {self.model_trainer_config.weight_name} --name yolov5s_results  --cache")
+            os.system(f"cd yolov5/ && python train.py --img 416 --batch {self.model_trainer_config.batch_size} --epochs {self.model_trainer_config.no_epochs} --data ../drive_data.yaml --cfg ./models/custom_yolov5s.yaml --weights {self.model_trainer_config.weight_name} --name yolov5s_results  --cache")
             os.system("cp yolov5/runs/train/yolov5s_results/weights/best.pt yolov5/")
             os.makedirs(self.model_trainer_config.model_trainer_dir, exist_ok=True)
             os.system(f"cp yolov5/runs/train/yolov5s_results/weights/best.pt {self.model_trainer_config.model_trainer_dir}/")
@@ -46,7 +46,7 @@ class ModelTrainer:
             os.system("rm -rf yolov5/runs")
             os.system("rm -rf train")
             os.system("rm -rf test")
-            os.system("rm -rf data.yaml")
+            os.system("rm -rf drive_data.yaml")
 
             model_trainer_artifact = ModelTrainerArtifact(
                 trained_model_file_path="yolov5/best.pt",
