3848d9eff9137ee3a392e5c94b716bff8093eecb
Parvez13 <prvzsohail@gmail.com>
2023-05-05 08:37:35 +0530

    Update dataset

--- cardetection/pipeline/training_pipeline.py
+++ cardetection/pipeline/training_pipeline.py
@@ -81,8 +81,7 @@ class TrainPipeline:
                 data_ingestion_artifact=data_ingestion_artifact
             )
             if data_validation_artifact.validation_status == True:
-                print("Done upto validation")
-             #    model_trainer_artifact = self.start_model_trainer()
+                model_trainer_artifact = self.start_model_trainer()
             #     #model_pusher_artifact = self.start_model_pusher(model_trainer_artifact=model_trainer_artifact,s3=self.s3_operations)
             
             else:
